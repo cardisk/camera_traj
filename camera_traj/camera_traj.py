5e3fc43b8b984dfbb455489500228b718f355624
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2026-03-22 22:55:29 +0100

    topics

--- camera_traj/camera_traj.py
+++ camera_traj/camera_traj.py
@@ -6,6 +6,23 @@ class CameraTraj(Node):
     def __init__(self):
         super().__init__('CameraTraj')
 
+        self.declare_parameters("depth_topic", "/zed/zed_node/depth/depth_registered")
+        self.declare_parameters("yolo_topic",  "/cone_detection/output")
+
+        self.depth_topic = self.get_parameter("depth_topic").get_parameter_value().string_value
+        self.yolo_topic  = self.get_parameter("yolo_topic").get_parameter_value().string_value
+
+        # self.subscription = self.create_subscription(
+        #     PoseStamped,
+        #     '/zed/zed_node/pose',
+        #     self.zed_slam_callback,
+        #     10)
+
+        # self.publisher = self.create_publisher(
+        #     PoseStampedFRT,
+        #     '/orb_slam3/camera_pose',
+        #     10)
+
         self.get_logger().info("Ready!")
 
 def main(args=None):
