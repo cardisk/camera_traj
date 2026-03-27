c71f7743cc3493398be90a7d0980f9dd7bc6767c
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2026-03-28 00:26:31 +0100

    Fixing compilation/launch issues

--- setup.py
+++ setup.py
@@ -12,19 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='cardisk',
     maintainer_email='matteo.cardinaletti03@gmail.com',
-    description='Local trajector on camera frame',
+    description='Trajectory on camera frame',
     license='TODO',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'camera_traj = camera_traj.camera_traj:main'
